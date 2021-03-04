a0f26ebe161b0e283020bb8d3b17644c46d09b26
anushan1508 <anushan1508@gmail.com>
2021-03-04 12:20:44 +0530

    almost filnished but somthing error with eval function

--- cal.js
+++ cal.js
@@ -1,8 +1,65 @@
-const calculator = document.querySelector(‘.calculator’)
-const keys = calculator.querySelector(‘.calculator__keys’)
+function getHistory(){
+	return document.getElementById("history-value").innerText;
+}
+function printHistory(num){
+	document.getElementById("history-value").innerText=num;
+}
 
-keys.addEventListener(‘click’, e => {
- if (e.target.matches(‘button’)) {
-   // Do something
- }
-})
+function getOutput(){
+	return document.getElementById("output").innerText;
+}
+
+}
+
+function printOutput(num){
+    if(num==""){
+        num=0;
+		document.getElementById("output").innerText=num;
+	}
+	else{
+		document.getElementById("output").innerText=num;
+	}	
+}
+printOutput("0");
+var operator =document.getElementsByClassName("key--operator");
+for(var i=0; i<operator.length;i++){
+    operator[i].addEventListener('click',function(){
+        if(this.id=="clear"){
+            printOutput("");
+        }
+        else if(this.id=="backspace"){
+            var output=getOutput().toString();
+            if(output){
+                output=output.substr(0,output.length-1);
+                printOutput(output);
+            }
+        }
+        else {
+            var output=getOutput();
+            var history=getHistory();
+            if(output!=""){
+                history=history+output;
+                if(this.id=="="){
+                    var result=eval(history);
+                    printOutput(result);
+
+                }
+                else{
+                    history=history+this.id;
+                    printOutput("");
+                }
+            }
+        }
+    });
+}
+
+var number =document.getElementsByClassName("key--number");
+for(var i=0; i<number.length;i++){
+    number[i].addEventListener('click',function(){
+        var output=getOutput();
+        if(output!=NaN){
+            output=output+this.id;
+            printOutput(output);
+        }
+    });
+}
