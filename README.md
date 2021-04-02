2705a18bceb1cf6069fba32568531c8ee77ad620
Anushan Santhirakumar <anushan1508@gmail.com>
2021-04-02 21:00:27 +0530

    finish

--- README.md
+++
@@ -1,3 +0,0 @@
-# simple_calculater
-This is for my Assignment Work. 
-I plan to use pure HTML,CSS &JS for this work. 
--- cal.js
+++ cal.js
@@ -12,31 +12,29 @@ function printOutput(num){
 		document.getElementById("output-value").innerText=num;
 	}
 	else{
-		document.getElementById("output-value").innerText=num;
+		document.getElementById("output-value").innerText=getFormattedNumber(num);
 	}	
 }
-printOutput("0");
-printHistory("0");
-    // function getFormattedNumber(num){
-    //     if(num=="-"){
-    //         return "";
-    //     }
-    //     var n = Number(num);
-    //     var value = n.toLocaleString("en");
-    //     return value;
-    // }
-    // function reverseNumberFormat(num){
-    //     return Number(num.replace(/,/g,''));
-    // }
+function getFormattedNumber(num){
+	if(num=="-"){
+		return "";
+	}
+	var n = Number(num);
+	var value = n.toLocaleString("en");
+	return value;
+}
+function reverseNumberFormat(num){
+	return Number(num.replace(/,/g,''));
+}
 var operator = document.getElementsByClassName("key--operator");
 for(var i =0;i<operator.length;i++){
 	operator[i].addEventListener('click',function(){
 		if(this.id=="clear"){
-			printHistory("0");
-			printOutput("0");
+			printHistory("");
+			printOutput("");
 		}
 		else if(this.id=="backspace"){
-			var output=getOutput().toString();
+			var output=reverseNumberFormat(getOutput()).toString();
 			if(output){
 				output= output.substr(0,output.length-1);
 				printOutput(output);
@@ -51,17 +49,17 @@ for(var i =0;i<operator.length;i++){
 				}
 			}
 			if(output!="" || history!=""){
-				output= output==""?output:(output);
+				output= output==""?output:reverseNumberFormat(output);
 				history=history+output;
 				if(this.id=="="){
 					var result=eval(history);
 					printOutput(result);
-					printHistory("0");
+					printHistory("");
 				}
 				else{
 					history=history+this.id;
 					printHistory(history);
-					printOutput("0");
+					printOutput("");
 				}
 			}
 		}
@@ -71,7 +69,7 @@ for(var i =0;i<operator.length;i++){
 var number = document.getElementsByClassName("key--number");
 for(var i =0;i<number.length;i++){
 	number[i].addEventListener('click',function(){
-		var output=getOutput();
+		var output=reverseNumberFormat(getOutput());
 		if(output!=NaN){ 
 			output=output+this.id;
 			printOutput(output);
